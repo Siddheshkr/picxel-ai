5dbe86a255c6a0857cadf73200351c43c5d0be4a
Siddhesh <siddheshkr21@gmail.com>
2025-08-02 21:30:39 +0530

    updated erro fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ClerkProvider
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
             appearance={{
               baseTheme: shadesOfPurple,
             }}
