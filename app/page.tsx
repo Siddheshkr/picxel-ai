5b6858b8baf989edcf0ec7daaf1ceb7d18e1df3d
Siddhesh <siddheshkr21@gmail.com>
2025-08-02 10:00:36 +0530

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
 import HerosSection from "@/components/ui/hero";
 
 export default function Home() {
   return (
     <div className="pt-28">
       {/* Hero section ====>>> */}
-     <HerosSection/>
+      <HerosSection />
       {/* Stats ====>>> */}
       {/* Features ====> */}
       {/* Pricing ====> */}
